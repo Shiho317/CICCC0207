79058b34ef47c094b0663f5d4faab04a7905def7
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-08 17:18:49 -0800

    added total price

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const itemsWrap = document.querySelector('.items');
 
 //get data from api
-
 const getData = (method, url, data) => {
   const datas = fetch(url, {
     method: method,
@@ -15,6 +14,7 @@ const getData = (method, url, data) => {
   return datas
 }
 
+//insert items to html
 async function getItems(){
   try {
     const items = await getData(
@@ -62,14 +62,13 @@ getItems()
 
 
 //get cart items
-
 const cartWrap = document.querySelector('.cart');
 const cartTitle = document.querySelector('.cart h1');
 let cartItems = [];
 const len = cartItems.length;
 const noItem = document.querySelector('.no-item');
-const totalWrap = document.querySelector('.total-wrap');
-
+let prices = [];
+let calcPrices = [];
 
 async function addToCart(id){
   
@@ -78,84 +77,106 @@ async function addToCart(id){
     `https://fakestoreapi.com/products/${id}`
   )
 
-  let amountNum = 0;
-
-  const itemDetails = document.createElement('div');
-  itemDetails.classList.add('item-details')
   cartItems.push(targetItem);
-  
-  // for(const cartItem of cartItems){
-  //   cartItem.amount = amountNum;
 
-  //   let total = cartItem.price;
-  //   const itemTotal = cartItem.price * cartItem.amount
-  //   total += itemTotal;
-  //   const totalPrice = document.createElement('h3');
-  //   totalPrice.textContent = `Total: C$${total}`
-  //   totalWrap.appendChild(totalPrice)
-  //   console.log(total)
-    
-  // }
-
+    prices.push(targetItem.price);
+      
+    //create elements
     const cartItemWrap = document.createElement('div');
-    cartItemWrap.classList.add('cart-item-wrap')
-    const itemTitle = document.createElement('h3');
-    itemTitle.textContent = targetItem.title;
-    const itemPrice = document.createElement('h4');
-    itemPrice.textContent = `C$ ${targetItem.price}`;
-    const amount = document.createElement('p');
-    amount.classList.add('amount');
-    const plusBtn = document.createElement('button');
-    plusBtn.textContent = '+';
-    plusBtn.classList.add('plus-btn')
-    const minusBtn = document.createElement('button');
-    minusBtn.textContent = '-';
-    minusBtn.classList.add('minus-btn');
-
+    cartItemWrap.classList.add('cart-item-wrap');
     cartWrap.appendChild(cartItemWrap);
-    itemDetails.appendChild(itemTitle);
-    itemDetails.appendChild(itemPrice);
-    const amountWrap = document.createElement('div');
-    amountWrap.classList.add('amount-wrap');
-    itemDetails.appendChild(amountWrap);
-    amount.textContent = amountNum;
-    amountWrap.appendChild(minusBtn);
-    amountWrap.appendChild(amount);
-    amountWrap.appendChild(plusBtn);
+
+    const totalWrap = document.querySelector('.total-wrap');
+
+    const itemDetails = document.createElement('div');
+    itemDetails.classList.add('item-details');
     cartItemWrap.appendChild(itemDetails);
+
     const itemImg = document.createElement('img');
     itemImg.setAttribute('src', targetItem.image);
     cartItemWrap.appendChild(itemImg);
 
+    const itemTitle = document.createElement('h3');
+    itemTitle.textContent = targetItem.title;
+    itemDetails.appendChild(itemTitle);
+
+    const itemPrice = document.createElement('h4');
+    itemPrice.textContent = `C$ ${targetItem.price}`;
+    itemDetails.appendChild(itemPrice);
+
+    const amountWrap = document.createElement('select');
+    amountWrap.classList.add('amount-wrap');
+    itemDetails.appendChild(amountWrap);
+
+    const subtotal = document.createElement('h3');
+    subtotal.classList.add('sub-total');
+    subtotal.textContent = `Subtotal: C$ ${targetItem.price}`
+    itemDetails.appendChild(subtotal);
+
+    const amount1 = document.createElement('option');
+    amountWrap.appendChild(amount1);
+    amount1.setAttribute('value', '1');
+    amount1.textContent = 1;
+
+    const amount2 = document.createElement('option');
+    amountWrap.appendChild(amount2);
+    amount2.setAttribute('value', '2');
+    amount2.textContent = 2;
+
+    const amount3 = document.createElement('option');
+    amountWrap.appendChild(amount3);
+    amount3.setAttribute('value', '3');
+    amount3.textContent = 3;
+
+    const amount4 = document.createElement('option');
+    amountWrap.appendChild(amount4);
+    amount4.setAttribute('value', '4');
+    amount4.textContent = 4;
+
+    const amount5 = document.createElement('option');
+    amountWrap.appendChild(amount5);
+    amount5.setAttribute('value', '5');
+    amount5.textContent = 5;
+
+
+    //calc price in cart
+    const calcPrice = () => {
+      const total = Number(amountWrap.value) * Number(targetItem.price);
+      console.log(amountWrap.value)
+      subtotal.textContent = `Subtotal: C$ ${total}`;
+      const tt = ((Number(totalWrap.textContent) - targetItem.price * targetItem.amount) + total).toFixed(2);
+      totalWrap.textContent = tt;
+
+      targetItem.amount = amountWrap.value;
+      console.log(targetItem)
+    }
+
+    console.log(subtotal.textContent)
+    amountWrap.addEventListener('change', calcPrice)
+
+    const initTotal = prices.reduce((sum, acc) => {
+      return sum + acc
+    })
+    
+    totalWrap.textContent = initTotal;
+    targetItem.amount = 1;
+    
     const btns = document.querySelectorAll('.buyButton');
     const unVaildBtn = btns[targetItem.id - 1]
     unVaildBtn.setAttribute('disabled', true)
     unVaildBtn.style.backgroundColor = 'grey';
     unVaildBtn.textContent = 'In your Cart';
     noItem.style.display = 'none';
+    const totalSec = document.querySelector('.total-sec');
+    totalSec.style.display = 'unset';
 
     //cart button num
     const cartBadge = document.querySelector('.fa-shopping-cart span')
     cartBadge.textContent = cartItems.length
 
-    //increase decrease amount
-
-  plusBtn.addEventListener('click', () => {
-    if(Number(amount.textContent) < 10)
-    amount.textContent = Number(amount.textContent) + 1;
-    targetItem.amount = Number(amount.textContent)
-    console.log(targetItem.amount)
-  })
-
-  minusBtn.addEventListener('click', () => {
-    if(Number(amount.textContent) > 0){
-      amount.textContent = Number(amount.textContent) - 1;
-      targetItem.amount = Number(amount.textContent)
-      console.log(targetItem.amount)
-    }
-  })
-
 }
+
+
 
 //cart modal
 
@@ -174,15 +195,3 @@ const closeCart = () => {
 
 closeBtn.addEventListener('click', closeCart)
 
-
-//total price
-  // const totalWrap = document.querySelector('.total-wrap');
-
-  // for(let i = 0; i < len; i++){
-  //   const itemTotal = cartItems[i].price * cartItems[i].amount
-  //   const total = total + itemTotal;
-  //   const totalPrice = document.createElement('h3');
-  //   totalPrice.textContent = `Total: C$${total}`
-  //   totalWrap.appendChild(totalPrice)
-  //   console.log(total)
-  // }
